3ac21abee8dcf3769806ce129e021cfa6fef37aa
mossroy <mossroy@mossroy.fr>
2015-05-01 18:19:43 +0200

    Add a few comments and .gitignore

--- maj_firmware_modem/pack_root_avec_maj_firmware/maj_pack_root_avec_firmware_modem.sh
+++ maj_firmware_modem/pack_root_avec_maj_firmware/maj_pack_root_avec_firmware_modem.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# This script generates a root pack for the ZTE Open C FR with an updated modem firmware
+# It downloads the 1.3B03 update from ZTE, extracts the modem firmware in it,
+# and updates it in the root pack from ZTE, generating a new root pack
+#
+# The reason for this script is ZTE did not grant us the right to redistribute
+# this modem firmware for use in the community builds
+#
+# For now, this is really quick and dirty
+# LGPL v3 license
+#
 wget http://download.ztedevice.com/UpLoadFiles/product/643/5522/soft/2015042909034130.zip
 unzip 2015042909034130.zip
 cd France\ OPEN\ C\ SD\ card\ upgrading\ instruction\ \&\ software\ package\(L\ leclerc\ telecom\)-268280B0304FFOS_FR_ZTE_OPENCV1.0.0B03
